02f65cef1bce6f78dd8a93c5924bafa87848fe73
chibani <oussamachibani.oc@gmail.com>
2016-05-02 22:03:12 +0200

    Initial commit

---
+++ app/src/main/java/com/chibani/alertesang/LancerAppel.java
@@ -0,0 +1,13 @@
+package com.chibani.alertesang;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LancerAppel extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lancer_appel);
+    }
+}
